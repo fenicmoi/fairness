c047fcd00857747387bbeafbc1d5423f59577664
fenicmmoi <phatthalung.it@gmail.com>
2021-04-05 09:23:17 +0700

    faq -update

--- faq.php
+++ faq.php
@@ -19,30 +19,75 @@
                 </div>
                 <div id="bootstrap-accordion_35" class="panel-group accordionStyles accordion" role="tablist" aria-multiselectable="true">
                     <div class="card mb-3">
+
                         <div class="card-header" role="tab" id="headingOne">
-                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse1_35" aria-expanded="false" aria-controls="collapse1">
-                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>อีเมลล์ของศูนย์ดำรงธรรมจังหวัดพัทลุงคืออะไร?</strong>
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><i class="fas fa-comment-dots fa-2x"></i> <strong>ศูนย์ดำรงธรรมจังหวัดพัทลุง คืออะไร มีหน้าที่อย่างไร?</strong>
                                 </h6>
                                 <span class="sign mbr-iconfont mbri-arrow-down"></span>
                             </a>
                         </div>
-                        <div id="collapse1_35" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_35">
+                        <div id="collapse1" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_35">
                             <div class="panel-body">
-                                <p class="mbr-fonts-style panel-text display-4">E-Mail = damrongthamptl@gmail.com</p>
+                                <p class="mbr-fonts-style panel-text display-4">
+                                <i class="fab fa-earlybirds fa-2x"></i>
+                                หน่วยงานที่จัดตั้งขึ้นในจังหวัดพัทลุง ตามประกาศคณะรักษาความสงบแห่งชาติ ฉบับที่ 96/2557
+                                เรื่อง การจัดตั้งศูนย์ดํารงธรรม  ทําหน้าที่ในการรับเรื่องร้องเรียนร้องทุกข์ ให้บริการข้อมูลข่าวสาร ให้คําปรึกษา รับเรื่องปัญหาความต้องการและข้อเสนอแนะของประชาชน และทําหน้าที่เป็นศูนย์บริการร่วมตามมาตรา ๓๒ แห่งพระราชกฤษฎีกาว่าด้วยหลักเกณฑ์และวิธีการ บริหารกิจการบ้านเมืองที่ดี พ.ศ. ๒๕๔๖ โดยมีผู้ว่าราชการจังหวัดพัทลุงเป็นผู้อำนวยการศูนย์ดำรงธรรมจังหวัดพัทลุง 
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="card-header" role="tab" id="headingtwo">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><i class="fas fa-comment-dots fa-2x"></i> <strong>สามารถร้องเรียน/ร้องทุกข์ต่อศูนย์ดำรงธรรมจังหวัดพัทลุงได้ทางช่องทางใดบ้าง?</strong>
+                                </h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_35">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">
+                                <i class="fab fa-earlybirds fa-2x"></i>
+                                : ศูนย์ดำรงธรรมจังหวัดพัทลุงมีช่องทางรับเรื่องร้องเรียน ดังนี้
+                                <ul>
+                                <ol>1.ร้องเรียนด้วยตนเอง(Walk in) ณ ศูนย์ดำรงธรรมจังหวัดพัทลุง ชั้น ๑ ศาลากลางจังหวัดพัทลุง ถนนราเมศวร์ ตำบลคูหาสวรรค์ อำเภอเมืองพัทลุง จังหวัดพัทลุง</ol>
+                                    <ol>2.ส่งจดหมาย มาที่ศูนย์ดำรงธรรมจังหวัดพัทลุง</ol>
+                                    <ol>3.เว็บไซต์ www.damrongdham.moi.go.th</ol>
+                                    <ol>4.แอปพลิเคชั่น 1567</ol>
+                                    <ol>5.แอปพลิเคชั่น Spond</ol>
+                                    <ol>6.แอปพลิเคชั่น Line  โดย แอดมาที่ไอดี  @360coqto</ol>
+                                    <ol>7.โทรศัพท์ 074 614 130 ตลอด 24 ชั่วโมง หรือโทร สายด่วน 1567</ol>
+                                </ul>                               
+                                 </p>
+                            </div>
+                        </div>
+
+                        <div class="card-header" role="tab" id="headingtree">
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><i class="fas fa-comment-dots fa-2x"></i> <strong>อีเมลล์ของศูนย์ดำรงธรรมจังหวัดพัทลุงคืออะไร?</strong>
+                                </h6>
+                                <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                            </a>
+                        </div>
+                        <div id="collapse3" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#collapse3">
+                            <div class="panel-body">
+                                <p class="mbr-fonts-style panel-text display-4">  <i class="fab fa-earlybirds fa-2x"></i>
+                                : damrongthamptl@gmail.com
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div class="card mb-3">
                         <div class="card-header" role="tab" id="headingOne">
-                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse2_35" aria-expanded="false" aria-controls="collapse2">
-                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>Application ของศูนย์ดำรงธรรมชื่อว่าอะไร?
+                            <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><i class="fas fa-comment-dots fa-2x"></i> <strong>Application ของศูนย์ดำรงธรรมชื่อว่าอะไร?
                                     </strong></h6>
                                 <span class="sign mbr-iconfont mbri-arrow-down"></span>
                             </a>
                         </div>
-                        <div id="collapse2_35" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_35">
+                        <div id="collapse4" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#collapse4">
                             <div class="panel-body">
-                                <p class="mbr-fonts-style panel-text display-4">
+                                <p class="mbr-fonts-style panel-text display-4"> <i class="fab fa-earlybirds fa-2x"></i> 
                                     ชื่อแอป spond และสามารถดูรายละเอียดเพิ่มเติม ได้ที่&nbsp;&nbsp;http://www.damrongdham.moi.go.th/mobileapp.pdf</p>
                             </div>
                         </div>
@@ -50,14 +95,14 @@
                     <div class="card mb-3">
                         <div class="card-header" role="tab" id="headingOne">
                             <a role="button" class="panel-title collapsed" data-toggle="collapse" data-core="" href="#collapse3_35" aria-expanded="false" aria-controls="collapse3">
-                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>เวลาทำการของศูนย์ดำรงธรรมคือช่วงใด?</strong></h6>
+                                <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><i class="fas fa-comment-dots fa-2x"></i> <strong>เวลาทำการของศูนย์ดำรงธรรมคือช่วงใด?</strong></h6>
                                 <span class="sign mbr-iconfont mbri-arrow-down"></span>
                             </a>
                         </div>
                         <div id="collapse3_35" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_35">
                             <div class="panel-body">
                                 <p class="mbr-fonts-style panel-text display-4">
-                                    เปิดทำการ วันจันทร์ - ศุกร์  ตั้งแต่เวลา 8:30 - 16:30 น.</p>
+                                <i class="fab fa-earlybirds fa-2x"></i>  เปิดทำการ วันจันทร์ - ศุกร์  ตั้งแต่เวลา 8:30 - 16:30 น.</p>
                             </div>
                         </div>
                     </div>
